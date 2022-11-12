ef2e05af0230d4b13a2ac21f5b8f30c1b886d5ff
kirshr <33167357+kirshr@users.noreply.github.com>
2022-11-12 11:38:20 -0700

    added the footer and map

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
+  const uluru = { lat: 53.600036, lng: -113.511243 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 12,
     center: uluru,
   });
   // The marker, positioned at Uluru
@@ -15,3 +15,5 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+
